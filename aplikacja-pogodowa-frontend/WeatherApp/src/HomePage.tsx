f944890041076788ac9d0829b87a86bf5b738e63
KacperNowakCode <kacper.nowakpraca1592@gmail.com>
2024-12-27 23:27:29 +0100

    styling

--- aplikacja-pogodowa-frontend/WeatherApp/src/HomePage.tsx
+++ aplikacja-pogodowa-frontend/WeatherApp/src/HomePage.tsx
@@ -1,6 +1,6 @@
-
-import { useState } from 'react';
+import { useState, CSSProperties } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from "./Styles.module.css";
 
 const HomePage = () => {
     const [location, setLocation] = useState('');
@@ -11,35 +11,35 @@ const HomePage = () => {
             navigate(`/${location}`);
         }
     };
+    
 
     return (
-        <div style={{}}>
-            <h1>Weather App</h1>
-            <input
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            placeholder="Enter location"
-            style={{ padding: '10px', width: '300px', fontSize: '16px' }}
-            onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                    handleSearch();
-                }
-            }}
-        />
-            <button
-                onClick={handleSearch}
-                style={{
-                    padding: '10px 20px',
-                    marginLeft: '10px',
-                    fontSize: '16px',
-                    cursor: 'pointer',
-                }}
-            >
-                Search
-            </button>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Weather App</h1>
+            <div className={styles.inputGroup}>
+                <input
+                    type="text"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    placeholder="Enter location"
+                    className={styles.input}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
+                />
+                <button
+                    onClick={handleSearch}
+                    className={styles.button}
+                >
+                    Search
+                </button>
+            </div>
+            <footer className={styles.footer}>Kacper Nowak & Mateusz Wątor</footer>
         </div>
     );
 };
+
 
 export default HomePage;
