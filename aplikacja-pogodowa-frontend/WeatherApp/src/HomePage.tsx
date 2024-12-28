3be0a7aac662ecb5e8d8202ff8cc475b0b4f9ba2
KacperNowakCode <kacper.nowakpraca1592@gmail.com>
2024-12-28 13:47:53 +0100

    visual changes

--- aplikacja-pogodowa-frontend/WeatherApp/src/HomePage.tsx
+++ aplikacja-pogodowa-frontend/WeatherApp/src/HomePage.tsx
@@ -1,7 +1,7 @@
-import { useState, CSSProperties } from 'react';
+import { useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from "./Styles.module.css";
-
+import JiggleIcon from './jiggleIcon.tsx';
 const HomePage = () => {
     const [location, setLocation] = useState('');
     const navigate = useNavigate();
@@ -15,8 +15,12 @@ const HomePage = () => {
 
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Weather App</h1>
+            <h1 className={styles.title}>
+                OOP Weather
+            </h1>
+            <JiggleIcon />
             <div className={styles.inputGroup}>
+            
                 <input
                     type="text"
                     value={location}
@@ -36,7 +40,24 @@ const HomePage = () => {
                     Search
                 </button>
             </div>
-            <footer className={styles.footer}>Kacper Nowak & Mateusz Wątor</footer>
+            <footer className={styles.footer}>
+                <a 
+                    href="https://github.com/KacperNowakCode" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: 'none', color: 'inherit', paddingRight: '5px' }}
+                >
+                    Kacper Nowak
+                </a> & 
+                <a 
+                    href="https://github.com/w0dur" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: 'none', color: 'inherit', paddingLeft: '5px' }}
+                >
+                    Mateusz Wątor
+                </a>
+            </footer>
         </div>
     );
 };
